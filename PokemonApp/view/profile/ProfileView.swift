1e9fddc96dc5a93344800fa1162ab5d0cd2e3541
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-05-30 18:09:32 +0700

    feat(profile-view): show user profile

---
+++ PokemonApp/view/profile/ProfileView.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileView.swift
+//  PokemonApp
+//
+//  Created by Wahyu Permadi on 30/05/20.
+//  Copyright © 2020 Wahyu Permadi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            Image("profile")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+                .frame(width: 250, height: 250)
+            Text("I Putu Wahyu Permadi")
+            Text("wahyu.permadi1725@gmail.com")
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
